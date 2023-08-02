f31440b0a9b90f6e613226b7f8345e747a6d9130
DucTai-261 <ductai126@gmail.com>
2023-08-03 01:59:57 +0700

    Refactor the path to the file

--- provisioning/roles/client/files/Basic_authen/basic_auth.sh
+++ provisioning/roles/client/files/Basic_authen/basic_auth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-URL="http://192.168.131.128/super_secret/index.html"
+URL="http://192.168.20.5/super_secret/index.html"
 USERNAME="user2"
-PASSWORD="HTTP_is_not_safe_!"
+PASSWORD="HTTP_is_not_safe"
 
 curl -s -o /dev/null -w "%{http_code}\n" --user "${USERNAME}:${PASSWORD}" "${URL}"
