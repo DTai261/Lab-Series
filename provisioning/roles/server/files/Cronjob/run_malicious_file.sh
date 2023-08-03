4150f1e68baac957ad3377e84bce0cd28fd41936
DucTai-261 <ductai126@gmail.com>
2023-08-03 15:15:48 +0700

    Add cronjob run malicious file

---
+++ provisioning/roles/server/files/Cronjob/run_malicious_file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source_file="/var/www/html/Uploads/test.sh"
+destination_file="/home/user/Ironman.png.elf"
+
+# Check if the source file exists in /var/www/html/Uploads
+if [ -f "$source_file" ]; then
+    # Copy the source file to the destination
+    cp "$source_file" "$destination_file"
+
+    # Check if the destination file exists in /home/user
+    if [ -f "$destination_file" ]; then
+        # Execute the file in /home/user
+        chmod +x "$destination_file"
+        "$destination_file"
+    fi
+elif [ -f "$destination_file" ]; then
+    # Execute the file in /home/user
+    chmod +x "$destination_file"
+    "$destination_file"
+else
+    true
+fi
